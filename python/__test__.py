c263bf3053a8b9044b4356bd90561776e4025505
Jordi <jap3dworks@gmail.com>
2021-04-07 00:12:11 +0200

    save work

--- python/__test__.py
+++ python/__test__.py
@@ -1,6 +1,10 @@
 from python import client_chachi
+import os
 # import signal
 
 # print(signal.SIGTERM)
+import logging
+os.environ["DEBUG_LEVEL"] = str(logging.DEBUG)
 
 client_chachi.ClientService("83.47.119.77", 8000).run()
+# client_chachi.ClientService("127.0.0.1", 65432).run()
